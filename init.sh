00d1df0ba3a3d256de6c870ca4ffc4e079c85238
Sean McCully <seancmccully@gmail.com>
2020-05-26 18:12:18 -0400

    adding init

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+chmod 0600 /ssh/ssh_host_rsa_key
+
+tee /ssh/sshd_config << EOF
+Port 9722
+AddressFamily any
+ListenAddress 0.0.0.0
+ListenAddress ::
+HostKey /ssh/ssh_host_rsa_key
+HostCertificate /ssh_mount/ssh_host_rsa_key-cert.pub
+PermitRootLogin no
+PasswordAuthentication no
+PermitEmptyPasswords no
+ChallengeResponseAuthentication no
+DenyUsers root bin daemon adm lp sync shutdown halt mail news uucp operator man postmaster cron ftp sshd at squid xfs games postgres cyrus vpopmail ntp smmsp guest nobody
+Match User bastion
+	AllowAgentForwarding no
+	AllowTcpForwarding yes
+	PermitOpen *:22
+	GatewayPorts no
+	X11Forwarding no
+	PermitTunnel no
+	ForceCommand echo 'Pritunl Zero Bastion Host'
+	TrustedUserCAKeys /ssh/trusted
+	AuthorizedPrincipalsFile /ssh/principals
+Match all
+EOF
+
+/usr/sbin/sshd -D -f /ssh/sshd_config
